a29f416b5f55314f8babcbb4ec07ccd62d378865
Patrick Kalkman <patrick.kalkman@exlrt.com>
2021-03-11 21:14:09 +0100

    added version number to __init__.py

--- tensorflow_helpers/tensorflow_helpers/__init__.py
+++ tensorflow_helpers/tensorflow_helpers/__init__.py
@@ -1,2 +1,9 @@
+"""Top-level package for tensorflow_helpers."""
+
+__author__ = """Patrick Kalkman"""
+__email__ = 'patrick@simpletechture.nl'
+__version__ = '0.2.0'
+
+
 from . import augmentation
 from . import training_preparation
